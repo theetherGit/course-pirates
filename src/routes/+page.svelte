c914bbf4574b83c82600d64f883ca921eabbf7fc
theetherGit <shivam@Shivams-Mac-mini.local>
2024-04-25 23:37:33 +0530

    Initial Setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<video
+        controls
+        src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+        width="620">
+    Sorry, your browser doesn't support embedded videos, but don't worry, you can
+    <a href="https://archive.org/details/BigBuckBunny_124">download it</a>
+    and watch it with your favorite video player!
+</video>
