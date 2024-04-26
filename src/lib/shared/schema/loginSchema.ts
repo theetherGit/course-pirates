343ab1a1609edabcdc39086a553ff9941bbb9c3a
Joyboy-02 <kumarritik262@gmail.com>
2024-04-26 22:04:11 +0530

    request-access-form

--- src/lib/shared/schema/loginSchema.ts
+++ src/lib/shared/schema/loginSchema.ts
@@ -1,8 +1,15 @@
-import * as z from "zod";
+import * as z from 'zod';
 
 export const loginSchema = z.object({
-    email: z.string({required_error: 'Email is required to login.'}).email('Invalid email').min(5).max(100),
-    password: z.string({required_error: 'Password is required to access your account.'}).min(8, 'You have very small').max(12, 'You got mine.')
-})
+	email: z
+		.string({ required_error: 'Email is required to login.' })
+		.email('Invalid email')
+		.min(5)
+		.max(100),
+	password: z
+		.string({ required_error: 'Password is required to access your account.' })
+		.min(8, 'You have very small')
+		.max(12, 'You got mine.')
+});
 
-export type LoginSchema = typeof loginSchema
+export type LoginSchema = typeof loginSchema;
